1f29aa839e023263d2471aefd0c02b0eb86c5713
yazhiniii20 <160877024+yazhiniii20@users.noreply.github.com>
2026-06-13 18:39:11 +0530

    latest update

--- src/main/java/com/yazh/smartspend/config/SecurityConfig.java
+++ src/main/java/com/yazh/smartspend/config/SecurityConfig.java
@@ -36,10 +36,7 @@ public class SecurityConfig {
   @Bean
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration =   new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of(
-    "http://localhost:5173",
-    "http://13.60.227.234",
-    "http://13.61.177.203"));
+    configuration.setAllowedOrigins(List.of("*"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
