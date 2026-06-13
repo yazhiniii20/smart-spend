6b065539139254cbe12a3c8ed1f5689f465e0231
yazhiniii20 <160877024+yazhiniii20@users.noreply.github.com>
2026-06-13 20:08:55 +0530

    Use Render backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://smart-spend-wwvn.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
